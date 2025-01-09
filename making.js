9d9b5d42f1f015254aecd1ba34f9459920b1c72d
YuukiAbe2023 <134372492+YuukiAbe2023@users.noreply.github.com>
2025-01-09 14:33:19 +0900

    Add files via upload

---
+++ making.js
@@ -0,0 +1,262 @@
+//これはグローバル変数のはず
+let standardSize = 30;
+let sideSlider;
+let tsubaSlider;
+
+let newWin;
+let matsu1;
+let matsu2;
+let matsu3;
+
+
+
+
+const BUTTON_CLICK_EVENT= document.getElementById('amizu_btn');
+
+let x;
+let y; 
+let z;
+
+let topSize;
+let sideSize;
+let tsubaSize;
+
+let scale = 5;
+
+var pass=function(){
+  // alert('(^-^)');
+
+    matsu1 = standardSize;
+    var num1 = matsu1;
+    //sessionStorageにデータを保存
+    sessionStorage.setItem("topData", matsu1);
+    
+    matsu2 = sideSlider.value;
+    var num2 = matsu2;
+    sessionStorage.setItem("sideData", matsu2);
+    
+    matsu3 = tsubaSlider.value;
+    var num3 = matsu3;
+    sessionStorage.setItem("tsubaData", matsu3);
+    
+}
+
+// ボタン選択（選択された値を保持する変数）
+let selectedValue = 1;//初期値は１倍
+
+function selectSize(selectedButton, size, value) {
+    // 全てのボタンの選択状態をリセット
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach(button => button.classList.remove("active"));
+
+    // クリックされたボタンを選択状態に設定
+    selectedButton.classList.add("active");
+
+    // 選択された値を更新
+    selectedValue = value;
+
+}
+
+
+//３Dモデルに丸みをつける
+function culculateZ(r, topSize,sideSize,ofset){
+  const roundStart=topSize-20;//丸め始める位置
+  const maxZ=10;//最大丸み
+  const minZ = -ofset;//基本のzの高さ
+  
+  if (r<roundStart){
+    return minZ;//丸ない範囲では一定
+  }
+  
+  const normalizedR=(r-roundStart)/(topSize-roundStart);//0~1に正規化
+  const t=Math.pow(normalizedR,3);//0~1の範囲で非線形に変化（平方で滑らかさを調整）
+  return minZ+maxZ*t;//高さを計算 
+}
+
+
+
+function setup() {
+  let container = document.getElementById('canvas-container');
+  // let canvas = createCanvas(windowWidth*0.5, windowHeight*0.9, WEBGL);
+  let canvas = createCanvas(700, 700, WEBGL);
+  canvas.parent(container); // canvasをcontainerの中に入れる
+
+  angleMode(DEGREES);
+  
+  // topSlider = document.getElementById('topSlider');
+  sideSlider = document.getElementById('sideSlider');
+  tsubaSlider = document.getElementById('tsubaSlider');
+  
+}
+
+
+
+
+
+function draw() {
+
+  background(240);
+  orbitControl(1,1,1);//マウスの動きに対する感度。1は初期値。
+  // line(-350, -350, 350, -350);
+  // line(-350, -350, -350, 350);
+  // line(350, 350, -350, 350);
+  // line(350, -350, 350, 350);
+  
+
+  rotateX(250);
+  rotateY(-20);
+  
+  
+  topSize = standardSize * scale;
+  // let topSize = Math.round(100 * selectedValue);
+  sideSize = (Math.round((parseInt(sideSlider.value) * selectedValue)/scale)) * scale;
+  tsubaSize = (Math.round((parseInt(tsubaSlider.value) * selectedValue)/scale)) * scale;
+  
+  // console.log(sideSize);
+  
+  let start=(Math.round((sideSize/1)/5))*5//中心からずらすため
+
+
+  // vertex配列をつくる
+  //トップクラウン
+  const topVertex = [];
+  for(let r = 0; r <= topSize; r += scale){ // 半径を10ずつ増やす
+    topVertex.push([]); // 二次元配列にする
+    
+    for(let phi =0; phi<360; phi+=5){
+      const x=r*cos(phi);
+      const y=r*sin(phi);
+      const z=culculateZ(r,topSize, sideSize,start);
+      
+      topVertex.at(-1).push({x, y, z}); // 二次元配列の最後に追加 
+          }   
+    }
+  //サイドクラウン上半分
+  const sideVertex = [];
+  // for(let h = 0; h <= sideSize + (10) ; h += 5){
+  //   sideVertex.push([]); // 二次元配列にする
+    
+  //   for(let phi = 0; phi < 360; phi += 5){ // 角度を10度ずつ増やす
+  //            let x = topSize * cos(phi);
+  //            let y = topSize * sin(phi);
+  //            let z = h;
+
+  //            sideVertex.at(-1).push({x, y, z}); // 二次元配列の最後に追加 
+  //         }    
+  //   }
+
+  for(let h2 = -start+(scale*2); h2 <= sideSize; h2 += 10){ 
+    sideVertex.push([]); // 二次元配列にする
+    
+    for(let phi = 0; phi < 360; phi += 5){ // 角度を10度ずつ増やす
+      if(h2 <= (sideSize -15)){
+             x = topSize * cos(phi);
+             y = topSize * sin(phi);
+             z = h2;
+      }else if(h2 >(sideSize - 15) && h2<= (sideSize - 10)){
+             x = (topSize + 2) * cos(phi) ;
+             y = (topSize + 2) * sin(phi);
+             z = h2;
+               
+      }else if(h2 >(sideSize - 10) && h2<= (sideSize - 5)){
+             x = (topSize + 5) * cos(phi) ;
+             y = (topSize +5) * sin(phi);
+             z = h2;
+      }else{
+        x = (topSize + 10) * cos(phi) ;
+             y = (topSize + 10) * sin(phi);
+             z = h2;
+             console.log(h2);
+      }
+             
+
+             sideVertex.at(-1).push({x, y, z}); // 二次元配列の最後に追加 
+          }    
+    }
+    
+  //つば
+  const tsubaVertex = [];
+  for(let r = topSize; r <= tsubaSize + topSize; r += scale){ // 半径を5ずつ増やす
+    tsubaVertex.push([]); // 二次元配列にする
+    
+    for(let phi = 0; phi < 360; phi += 5){ // 角度を5度ずつ増やす
+             let x = (r+10) * cos(phi);
+             let y = (r+10) * sin(phi);
+             let z = (sideSize + r -topSize);
+             tsubaVertex.at(-1).push({x, y, z}); // 二次元配列の最後に追加 
+    }    
+  }
+  // console.log(hatVertex);
+  
+  //描写の関数
+  function drawVertexShapes(vertices) {
+    for (let r = 0; r < vertices.length; r++) {
+      for (let phi = 0; phi < vertices[r].length; phi++) {
+        if (r < vertices.length - 1 && phi < vertices[r].length - 1) {
+          beginShape();
+          fill(237, 255, 96);
+          stroke(0);
+          strokeWeight(0.5);
+          vertex(vertices[r][phi].x, vertices[r][phi].y, vertices[r][phi].z);
+          vertex(vertices[r + 1][phi].x, vertices[r + 1][phi].y, vertices[r + 1][phi].z);
+          vertex(vertices[r + 1][phi + 1].x, vertices[r + 1][phi + 1].y, vertices[r + 1][phi + 1].z);
+          vertex(vertices[r][phi + 1].x, vertices[r][phi + 1].y, vertices[r][phi + 1].z);
+          endShape(CLOSE);
+        } else if (r < vertices.length - 1 && phi === vertices[r].length - 1) {
+          beginShape();
+          vertex(vertices[r][phi].x, vertices[r][phi].y, vertices[r][phi].z);
+          vertex(vertices[r][0].x, vertices[r][0].y, vertices[r][0].z);
+          vertex(vertices[r + 1][0].x, vertices[r + 1][0].y, vertices[r + 1][0].z);
+          vertex(vertices[r + 1][phi].x, vertices[r + 1][phi].y, vertices[r + 1][phi].z);
+          endShape(CLOSE);
+        }
+      }
+    }
+  }
+  
+  //トップクラウンの描写
+  drawVertexShapes(topVertex);
+  // サイドクラウンの描写
+  drawVertexShapes(sideVertex);
+  // つばの描写
+  drawVertexShapes(tsubaVertex);
+
+}
+
+
+
+
+//スライダーのデザイン
+document.addEventListener('DOMContentLoaded', () => {
+ 
+  // 全てのレンジスライダーの要素を取得（必要に応じてセレクタを変更）
+  const rangeSliders = document.querySelectorAll('input[type="range"]');
+  // Track の元の色
+  const baseColor = '#fff';
+  // Track のつまみの左側の部分の色
+  const activeColor = '#000';
+ 
+  // 取得したレンジスライダーの各要素に対して実行
+  rangeSliders.forEach((slider) => {
+    // input イベントのリスナーを設定
+    slider.addEventListener('input', (e) => {
+      // updateSlider を呼び出す
+      updateSlider(e.target);
+    });
+    // updateSlider を実行して現在の値を反映
+    updateSlider(slider);
+  });
+ 
+  // input イベントで呼び出される関数（トラックの塗りの範囲と色を設定する関数）
+  function updateSlider(slider) {
+    // max 属性の値が省略されている場合は100を設定
+    if(!slider.max) {
+      slider.max = 100;
+    }
+    // 現在の値から割合（%）を取得
+    const progress = (slider.value / slider.max) * 100;
+    // linear-gradient で Track の色を設定
+    slider.style.background = `linear-gradient(to right, ${activeColor} ${progress}%, ${baseColor} ${progress}%)`;
+  }
+ 
+});
---
+++ result.js
@@ -0,0 +1,261 @@
+let canvas;
+
+//sessionStorageに保存したデータを取得
+let topText = sessionStorage.getItem("topData");
+
+//文字列を数字にする
+let topNum = Number(topText);
+
+let sideText = sessionStorage.getItem("sideData");
+let sideNum = Number(sideText);
+
+let tsubaText = sessionStorage.getItem("tsubaData");
+let tsubaNum = Number(tsubaText);
+
+let topDansuu = Math.round(topNum);//0.06のとこは後で変える！//四捨五入
+let topDansuuOffset = topDansuu + 1;
+let sideDansuu = Math.round(sideNum * 0.17);
+let tsubaDansuu = Math.round(tsubaNum * 0.17);
+console.log(topNum);
+console.log(sideDansuu);
+
+
+
+//表作成
+// 表データをJavaScript変数に格納
+const tableData = [
+  ["", "段数", "増し目"],
+  ["トップ", topDansuu, ""],
+  ["サイド", sideDansuu, ""],
+  ["つば", tsubaDansuu, ""]
+];
+
+// 表を描画する関数
+function renderTable(data) {
+  const table = document.getElementById("data-table");
+  table.innerHTML = ""; // 既存の表をクリア
+
+  data.forEach((row, rowIndex) => {
+    const tr = document.createElement("tr"); // 行を作成
+
+    row.forEach((cell) => {
+      const cellElement = rowIndex === 0 ? document.createElement("th") : document.createElement("td");
+      cellElement.textContent = cell; // セルにデータを設定
+      tr.appendChild(cellElement); // 行にセルを追加
+    });
+
+    table.appendChild(tr); // テーブルに行を追加
+  });
+}
+
+// 初期表示
+renderTable(tableData);
+
+//編み図のサイズ
+const size = 6;
+const centerCircleSize = size * 6;
+const halfSize = size / 2;
+const amimes = [
+  [0],//1段目
+];
+
+//トップ（2段目以降）の配列作成
+for (let i = 1; i < topDansuu; i++) {
+  let amime = new Array(i)
+  amimes.push(amime);
+
+  if (amime.length % 2 == 0) {
+    for (let a = 0; a < amime.length - 1; a++) {
+      amime[a] = 0;
+    }
+    amime[amime.length - 1] = 1;
+
+  } else {
+    for (let a = 0; a < amime.length; a++) {
+      let m = (amime.length / 2) - 1 / 2;
+
+      if (a == m) {
+        amime[a] = 1;
+      } else {
+        amime[a] = 0;
+      }
+
+    }
+  }
+}
+
+//サイドの配列作成
+for (let i = 0; i < sideDansuu; i++) {
+  let amime = new Array(topDansuu - 1);
+  for (let a = 0; a < amime.length; a++) {
+    amime[a] = 0;
+  }
+  amimes.push(amime);
+}
+
+//つばの配列作成
+for (let i = topDansuu; i < topDansuu + tsubaDansuu; i++) {
+  let amime = new Array(i);
+  //  for(let a = 0; a < amime.length; a ++){
+  //   amime[a] = 0;
+  // }
+  amimes.push(amime);
+
+  if (amime.length % 2 == 0) {
+    for (let a = 0; a < amime.length - 1; a++) {
+      amime[a] = 0;
+    }
+    amime[amime.length - 1] = 1;
+  } else {
+    for (let a = 0; a < amime.length; a++) {
+      let m = (amime.length / 2) - 1 / 2;
+      if (a == m) {
+        amime[a] = 1;
+      } else {
+        amime[a] = 0;
+      }
+    }
+  }
+}
+
+// console.log(amimes);//コンソール出力
+
+
+
+
+//一度だけ実行
+function setup() {
+  canvas = createCanvas(windowWidth - 300, windowHeight - 30);
+  canvas.class("canvas");
+  angleMode(DEGREES);
+
+}
+
+
+function koma(x, y, angle) {
+  // const size = 10;
+  // const halfSize = size / 2;
+  push();
+  translate(x, y);
+  rotate(angle);
+  line(-halfSize, -halfSize, halfSize, halfSize);
+  line(halfSize, -halfSize, -halfSize, halfSize);
+  pop();
+}
+
+function mashime(x, y, angle) {
+  // const size = 10;
+  // const halfSize = size / 2;
+  const offset = 2;
+
+  push();
+  translate(x, y);
+  rotate(angle);
+  strokeWeight(1.5);
+  line(-halfSize, -halfSize, halfSize, halfSize);
+  line(halfSize, -halfSize, -halfSize, halfSize);
+  line(-offset - size, 0, -offset, -size);
+  line(-offset - size, 0, -offset, size);
+  pop();
+}
+
+function drawAmime(x, y, phi, amime) {
+  if (amime === 0) {
+    koma(x, y, phi);
+  } else if (amime === 1) {
+    mashime(x, y, phi);
+  }
+}
+
+function drawAmimes(amimes, phi, r) {
+  for (const [i, amime] of amimes.entries()) {
+    const angle = phi + map(i, -1, amimes.length, 30, -30);
+    const x = r * cos(angle);
+    const y = r * sin(angle);
+    drawAmime(x, y, angle, amime);
+  }
+}
+
+
+
+
+
+//繰り返し実行
+function draw() {
+  // background(220);
+  clear();
+  translate(width - 50, height - 50);//50ってのは適当です
+  noFill();
+
+  push();
+  text("aiueo", 200, 200);
+  textSize(200);
+  fill(0);
+  pop();
+
+  //帽子の枠組の定義
+  let topMax = topDansuuOffset * (size * 6);//この6ってどっかと繋がってる？
+  let sideMax = topMax + sideDansuu * (size * 6);
+  let rMax = sideMax + tsubaDansuu * (size * 6);
+  let lineMax = rMax / 2;
+  let centerLine = centerCircleSize / 2;
+
+  //線の描写
+  line(centerCircleSize * cos(180), centerCircleSize * sin(180), lineMax * cos(180), lineMax * sin(180));
+
+  line(centerCircleSize * cos(240), centerCircleSize * sin(240), lineMax * cos(240), lineMax * sin(240));
+
+
+  //中心の六等分の線の描写
+  for (let phi = 0; phi <= 360; phi += 60) {
+    line(centerLine * cos(phi), centerLine * sin(phi), centerCircleSize * cos(phi), centerCircleSize * sin(phi));
+  }
+
+  //「わ」の描写
+  push();
+  strokeWeight(2);
+  circle(0, 0, centerCircleSize);
+  pop();
+  //一段目の円の描写
+  circle(0, 0, centerCircleSize * 2);
+
+  //他の円弧の描写
+  for (let r = centerCircleSize * 3; r <= rMax; r += centerCircleSize) {
+    if ((r == topMax) || (r == sideMax) || (r == rMax)) {
+      push();
+      strokeWeight(2);
+      arc(0, 0, r, r, 180, 240);
+      pop();
+    } else {
+      arc(0, 0, r, r, 180, 240);
+    }
+  }
+
+  //立ち上がり記号と引き抜き記号の描写
+  for (let r = centerCircleSize / 2 + (3 / 2) * size; r < rMax / 2; r += 3 * size) {
+    push();
+    rotate(-30);
+    ellipse(0, -r, size, size * 2);
+    fill('black');
+    ellipse(0 + 5, -r - 7, size / 2, size / 2);
+    pop();
+  }
+
+  //編み図記号の描写
+  for (let i = 1; i < topDansuuOffset + sideDansuu + tsubaDansuu; i++) {
+    let centerOffset = centerCircleSize * (3 / 4)
+    let r = centerOffset + (centerCircleSize / 2) * (i - 1);
+    let arrayNumber = ((r - centerOffset) / (centerCircleSize / 2));
+
+    if (r == centerOffset) {
+      for (phi = 30; phi < 390; phi += 60) {
+        drawAmimes(amimes.at(arrayNumber), phi, r);
+      }
+    } else {
+      for (let phi = 210; phi < 270; phi += 60) {
+        drawAmimes(amimes.at(arrayNumber), phi, r);
+      }
+    }
+  }
+  
+}
